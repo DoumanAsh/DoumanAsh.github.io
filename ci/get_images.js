36925ba57cb02f4f1d5fe7b105a04496feccaeb0
Douman <douman@gmx.se>
2017-01-31 19:09:39 +0300

    Add re-try for imgur data generator

--- ci/get_images.js
+++ ci/get_images.js
@@ -11,26 +11,36 @@ const options = {
     }
 };
 
-http.get(options, function(response){
-    let data = '';
+const DELAY = 60000; //1minute
 
-    if (response.statusCode !== 200) {
-        console.error("Cannot access imgur album. Status code %d", response.statusCode);
-        return;
-    }
+function generate_data() {
+    http.get(options, function(response) {
+        let data = '';
 
-    response.setEncoding('utf8');
-    response.on('data', (chunk) => {
-        data += chunk;
-    });
-    response.on('end', () => {
-        const result_file = path.join(__dirname, '..', 'waifu.json');
-        const json_data = JSON.parse(data);
-        fs.writeFile(result_file, JSON.stringify(json_data, null, 4), 'utf-8', () => {
-            console.log("Waifu data is prepared");
+        if (response.statusCode !== 200) {
+            console.error("Cannot access imgur album. Status code %d", response.statusCode);
+            console.log("retry in %s seconds...", DELAY / 1000);
+            setTimeout(generate_data, DELAY);
+            return;
+        }
+
+        response.setEncoding('utf8');
+        response.on('data', (chunk) => {
+            data += chunk;
         });
-    });
+        response.on('end', () => {
+            const result_file = path.join(__dirname, '..', 'waifu.json');
+            const json_data = JSON.parse(data);
+            fs.writeFile(result_file, JSON.stringify(json_data, null, 4), 'utf-8', () => {
+                console.log("Waifu data is prepared");
+            });
+        });
 
-}).on("error", function(e){
-    console.error("Got error: " + e.message);
-});
+    }).on("error", function(e){
+        console.error("Got error: " + e.message);
+        console.log("retry in %s seconds...", DELAY / 1000);
+        setTimeout(generate_data, DELAY);
+    });
+}
+
+generate_data();
